5e44bbe0f6d31027338d8ee5dd27acf9df68c94e
unknown <elgdiab@gmail.com>
2024-12-16 02:01:15 +0200

    Finishing the 04-Getting Started section.

---
+++ 04-Getting Started/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+// using namespace std;
+
+int main() {
+    int favorite_number;
+	std::cout << "Enter your favorite number between 1 to 100: ";
+	std::cin >> favorite_number; 
+    std::cout << "Amazing!! That is my favorite number too!" << std::endl;
+    std::cout << "No really!!, " << favorite_number << " is my favorite number!"  << std::endl;
+    return 0;
+}
