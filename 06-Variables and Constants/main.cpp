05b56d50c7edf7122c8ba6c570f05fde0728a20d
unknown <elgdiab@gmail.com>
2024-12-26 17:47:42 +0200

    Adding the 06-Variables and Constants section.

---
+++ 06-Variables and Constants/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+ using namespace std;
+
+int main()
+{
+    const double price_per_small_room {25};
+    const double price_per_large_room {35};
+    const float tax {0.06};
+    const int estimate_time {30}; // Days
+    
+    cout << "Hello, Welcome to Eldiaby's Carpet Cleaning Service \n\n";
+    
+    cout << "How many small rooms would you like to clean? ";
+    int small_rooms;
+    cin >> small_rooms;
+    
+    cout << "How many large rooms would you like to clean? ";
+    int large_rooms;
+    cin >> large_rooms;
+    
+    cout << "Number of small rooms: " << small_rooms <<endl;
+    cout << "Number of large rooms: " << large_rooms <<endl;
+    cout << "Price per small room: $" << price_per_small_room << endl;
+    cout << "Price per large room: $" << price_per_large_room << endl;
+    
+    double total_cost =(price_per_small_room * small_rooms) + (price_per_large_room * large_rooms);
+    cout << "Cost: $" << total_cost <<endl;
+    
+    float bill_tax = total_cost * tax;
+    cout << "Tax: $" << bill_tax <<endl;
+    
+    cout << "=======================================\n";
+    
+    double total_estimate = total_cost + bill_tax;
+    cout << "Total estimate: $" << total_estimate << endl;
+    cout << "This estimate is valid for " << estimate_time << " days" << endl;
+    
+	return 0;
+}
